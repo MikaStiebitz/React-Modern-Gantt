677c73d49efadda87df11f1b20d3af87ef6b92f6
Mika Stiebitz <mika.stiebitz@tradebyte.com>
2025-03-22 19:14:24 +0100

    blub

--- dist/nextjs/index.d.ts
+++ dist/nextjs/index.d.ts
@@ -2,3 +2,4 @@ export { NextGanttChart } from "./NextGanttChart";
 export * from "../types";
 export * from "../utils";
 export * from "../services";
+export { GanttThemeProvider, useGanttTheme } from "../components/core";
